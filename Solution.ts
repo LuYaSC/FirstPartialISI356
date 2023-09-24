8e595240aba34827b7f800c3345231d8416731d2
Luis Jose Jimenez Peña <Luis.Jimenez.extern@arvato-scs.com>
2023-09-24 19:36:30 -0400

    Test Solution

--- Solution.ts
+++ Solution.ts
@@ -29,3 +29,123 @@ Puedes anadir logs de info, warning y error en las llamadas, para un mejor contr
 
 Diseño por Contrato (Opcional):
 Puedes anadir validaciones en precondiciones o postcondiciones como lo veas necesario*/
+
+interface IBook {
+    title: string;
+    author: string;
+    ISBN: string;
+}
+
+interface IEmailService {
+    sendEmail(email: string, message: string): void;
+}
+
+class EmailService implements IEmailService {
+    sendEmail(email: string, message: string): void {
+        console.log(`Sending email to ${email}: ${message}`);
+        // Real email sending code would go here
+    }
+}
+
+class Book implements IBook {
+    constructor(public title: string, public author: string, public ISBN: string) {}
+}
+
+class BookBuilder {
+    private title: string = '';
+    private author: string = '';
+    private ISBN: string = '';
+
+    setTitle(title: string): BookBuilder {
+        this.title = title;
+        return this;
+    }
+
+    setAuthor(author: string): BookBuilder {
+        this.author = author;
+        return this;
+    }
+
+    setISBN(ISBN: string): BookBuilder {
+        this.ISBN = ISBN;
+        return this;
+    }
+
+    build(): Book {
+        return new Book(this.title, this.author, this.ISBN);
+    }
+}
+
+interface IObserver {
+    notify(message: string): void;
+}
+
+class Loan {
+    constructor(public ISBN: string, public userID: string, public date: Date) {}
+}
+
+class LibraryManager {
+    private books: Book[] = [];
+    private loans: Loan[] = [];
+    private static instance: LibraryManager;
+    private emailService: IEmailService;
+
+    private constructor(emailService: IEmailService) {
+        this.emailService = emailService;
+    }
+
+    // Singleton
+    public static getInstance(emailService: IEmailService): LibraryManager {
+        if (!this.instance) {
+            this.instance = new LibraryManager(emailService);
+        }
+        return this.instance;
+    }
+
+    // Observer pattern
+    private observers: IObserver[] = [];
+
+    addObserver(observer: IObserver) {
+        this.observers.push(observer);
+    }
+
+    private notifyAll(message: string) {
+        this.observers.forEach(observer => observer.notify(message));
+    }
+
+    addBook(book: Book) {
+        this.books.push(book);
+        this.notifyAll(`New book added: ${book.title}`);
+    }
+
+    loanBook(ISBN: string, userID: string) {
+        const book = this.books.find(b => b.ISBN === ISBN);
+        if (book) {
+            this.loans.push(new Loan(ISBN, userID, new Date()));
+            this.emailService.sendEmail(userID, `You borrowed the book ${book.title}`);
+        }
+    }
+
+    // You can add other methods (removeBook, returnBook, etc.) adjusting where necessary.
+}
+
+class User implements IObserver {
+    constructor(public id: string, public email: string) {
+        LibraryManager.getInstance(new EmailService()).addObserver(this);
+    }
+
+    notify(message: string) {
+        console.log(`User ${this.id} notified: ${message}`);
+    }
+}
+
+const library = LibraryManager.getInstance(new EmailService());
+const user1 = new User('user01', 'user01@example.com');
+const book1 = new BookBuilder()
+    .setTitle("The Great Gatsby")
+    .setAuthor("F. Scott Fitzgerald")
+    .setISBN("123456789")
+    .build();
+
+library.addBook(book1);
+library.loanBook("123456789", "user01@example.com");
